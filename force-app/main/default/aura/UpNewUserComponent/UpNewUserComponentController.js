0e34c0f6e2edae7fd61108c1b9f16632d0afefa1
Don Rivard <drivard@demandware.com>
2016-12-09 09:14:43 -0500

    removed .appcloude and added a .gitignore file

--- force-app/main/default/aura/UpNewUserComponent/UpNewUserComponentController.js
+++ force-app/main/default/aura/UpNewUserComponent/UpNewUserComponentController.js
@@ -1,4 +1,13 @@
+
 ({
-	myAction : function(component, event, helper) {
-	}
+	 handleAccount : function(component, event, helper) {
+        console.log('UpNewUserComponent:handleAccountSelected:');
+        var account = event.getParam('account');
+        console.log('UpNewUserComponent account: ', account);
+        console.log('UpNewUserComponent account ID : ', account.Id);
+
+        component.set('v.account', account);
+        component.set('v.accountId', account.Id);
+    }
+
 })
