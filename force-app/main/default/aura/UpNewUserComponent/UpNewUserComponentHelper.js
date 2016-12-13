07ba034a4eaf8556995396935d76b804015c37bd
Don Rivard <drivard@demandware.com>
2016-12-13 13:00:37 -0500

    Changed the Account list to a pick list. Adjusted the UI to support the list

--- force-app/main/default/aura/UpNewUserComponent/UpNewUserComponentHelper.js
+++ force-app/main/default/aura/UpNewUserComponent/UpNewUserComponentHelper.js
@@ -1,4 +1,20 @@
 ({
-	helperMethod : function() {
-	}
+	showAccountBrowser : function(component) {
+		$A.util.removeClass(component.find('accounts'), 'slds-hide');
+        $A.util.addClass(component.find('accounts'), 'slds-show');
+	
+        $A.util.removeClass(component.find('accountdetail'), 'slds-hide');
+        $A.util.addClass(component.find('accountdetail'), 'slds-show');
+	
+	},
+	
+	hideAccountBrowser : function(component) {
+		$A.util.removeClass(component.find('accounts'), 'slds-show');
+        $A.util.addClass(component.find('accounts'), 'slds-hide');
+	
+        $A.util.removeClass(component.find('accountdetail'), 'slds-show');
+        $A.util.addClass(component.find('accountdetail'), 'slds-hide');
+	
+	},
+	
 })
